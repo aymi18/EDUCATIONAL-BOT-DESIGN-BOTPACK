76514b354b4dd810d5b65887a3b2329a24733daf
yoya <162847989+aymi18@users.noreply.github.com>
2024-04-05 10:12:28 +0800

    Update ai.js

--- modules/commands/ai.js
+++ modules/commands/ai.js
@@ -5,7 +5,7 @@ const storageFile = 'user_data.json';
 const chatRecordFile = 'chat_records.json';
 
 module.exports.config = {
-    name: "ai",
+    name: "yela",
     version: "1.0.0",
     hasPermssion: 0,
     credits: "Jonell Magallanes",
@@ -24,7 +24,7 @@ module.exports.run = async function ({ api, event, args }) {
     if (!content) return api.sendMessage("Please provide your question.\n\nExample: ai what is the solar system?", event.threadID, event.messageID);
 
     try {
-        api.sendMessage("🔍 | AI is searching for your answer. Please wait...", event.threadID, event.messageID);
+        api.sendMessage("🔍 | Wait hanapan kita sagot...", event.threadID, event.messageID);
 
         const response = await axios.get(apiUrl);
         const { content: result, requestCount } = response.data; // Updated response structure
@@ -48,7 +48,7 @@ module.exports.run = async function ({ api, event, args }) {
         api.sendMessage(responseMessage, event.threadID, event.messageID);
     } catch (error) {
         console.error(error);
-        api.sendMessage("An error occurred while processing your request.", event.threadID);
+        api.sendMessage("Hala beh d ko mahanap yunh sagot may error.", event.threadID);
     }
 };
 
